4e285285480fdcb09f00a72d41dacb77f49295d1
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-08 13:43:02 +0000

    tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function createIteratorObject(report) {
+  // Generator function to yield each employee from each department
+  function* employeeGenerator() {
+    for (const department of Object.keys(report.allEmployees)) {
+      for (const employee of report.allEmployees[department]) {
+        yield employee;
+      }
+    }
+  }
+  return employeeGenerator();
+}
