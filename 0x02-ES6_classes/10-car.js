f5726699ab7e1df4f70306b9cd46bae1cc2ccb0e
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-11 16:51:09 +0000

    100%?

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, model, color) {
+    this._brand = brand;
+    this._model = model;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const clone = new this.constructor();
+    const symbols = Object.getOwnPropertySymbols(this);
+    symbols.forEach((symbol) => {
+      clone[symbol] = this[symbol];
+    });
+    return clone;
+  }
+}
