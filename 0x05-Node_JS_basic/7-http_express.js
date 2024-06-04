56c1042bec0e0d568d3d140f8414f5cf31bbf195
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-04 22:51:16 +0100

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,17 +4,17 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-    res.write('This is the list of our students\n');
-    try {
-        await countStudents(process.argv[2]);
-    } catch (err) {
-        res.write(err.message);
-    }
-    res.end();
+  res.write('This is the list of our students\n');
+  try {
+    await countStudents(process.argv[2]);
+  } catch (err) {
+    res.write(err.message);
+  }
+  res.end();
 });
 
 app.listen(1245);
