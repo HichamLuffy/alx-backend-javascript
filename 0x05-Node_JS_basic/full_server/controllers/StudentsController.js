5b9d6be18bae1c8223a208a936517aa1eae88d38
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-04 22:59:45 +0100

    full server

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+const readDatabase = require('../utils');
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const fields = await readDatabase(process.argv[2]);
+      res.write('This is the list of our students\n');
+      for (const [field, names] of Object.entries(fields)) {
+        res.write(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`);
+      }
+      res.status(200).end();
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+    if (!['CS', 'SWE'].includes(major)) {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    try {
+      const fields = await readDatabase(process.argv[2]);
+      const names = fields[major] || [];
+      res.status(200).send(`List: ${names.join(', ')}`);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+}
+
+module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+app.use('/', router);
+
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
