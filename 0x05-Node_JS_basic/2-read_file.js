fc4efed1219db884767375fc678d9211e9109828
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-04 22:27:57 +0100

    fixing task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,24 +4,30 @@ function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1);
-    const fields = {};
 
-    students.forEach((student) => {
-      const [firstName, , field] = student.split(',');
-      if (!fields[field]) fields[field] = [];
-      fields[field].push(firstName);
-    });
-
-    console.log(`Number of students: ${students.length}`);
-    for (const [field, names] of Object.entries(fields)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.split('\n');
+  const hashtable = {};
+  let students = -1;
+  for (const line of lines) {
+    if (line.trim() !== '') {
+      const columns = line.split(',');
+      const field = columns[3];
+      const firstname = columns[0];
+      if (students >= 0) {
+        if (!Object.hasOwnProperty.call(hashtable, field)) {
+          hashtable[field] = [];
+        }
+        hashtable[field] = [...hashtable[field], firstname];
+      }
+      students += 1;
     }
-  } catch (err) {
-    throw new Error('Cannot load the database');
+  }
+  console.log(`Number of students: ${students}`);
+  for (const key in hashtable) {
+    if (Object.hasOwnProperty.call(hashtable, key)) {
+      console.log(`Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}`);
+    }
   }
 }
 
