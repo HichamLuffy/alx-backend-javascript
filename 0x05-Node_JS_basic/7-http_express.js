0cbcf0f9b95330372f642b131931a22851b9a07c
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-04 22:55:23 +0100

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,20 +1,56 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const app = express();
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const hashtable = {};
+        let students = -1;
+        let result = '';
+        for (const line of lines) {
+          if (line.trim() !== '') {
+            const columns = line.split(',');
+            const field = columns[3];
+            const firstname = columns[0];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
+              }
+              hashtable[field] = [...hashtable[field], firstname];
+            }
+            students += 1;
+          }
+        }
+        result += `Number of students: ${students}\n`;
+        for (const key in hashtable) {
+          if (Object.hasOwnProperty.call(hashtable, key)) {
+            result += `Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}\n`;
+          }
+        }
+        resolve(result);
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.write('This is the list of our students\n');
-  try {
-    await countStudents(process.argv[2]);
-  } catch (err) {
-    res.write(err.message);
-  }
-  res.end();
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res.status(500).send(`This is the list of our students\n${error.message}`);
+    });
 });
 
 app.listen(1245);
