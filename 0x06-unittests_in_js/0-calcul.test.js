0a56ebad4d7eeb1d6ba915915b6c6b0b78e102ce
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-07 03:33:35 +0100

    fix task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,12 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
-  it('should return sum of rounded numbers', function() {
-    assert.strictEqual(calculateNumber(1.4, 2.5), 4);
+describe('calculateNumber', () => {
+  it('should return -4 when inputs are -1 and -2.7', () => {
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
+  });
+
+  it('should return -4 when inputs are -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
   });
 });
