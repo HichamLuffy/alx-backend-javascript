43e8a7a22ecce3a1adad9d7fe4b0a3e7c30e4849
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-07 03:33:12 +0100

    fix test and tasks

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+  it('should return sum of rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 2.5), 4);
+  });
+  it('should return difference of rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 2.5), -1);
+  });
+  it('should return division of rounded numbers', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 4.4, 2.2), 2);
+  });
+  it('should return error when dividing by zero', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 4.4, 0), 'Error');
+  });
+});
